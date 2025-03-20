5fa11ec88e63c8843c9c9ce1ad136794dba670ce
Yash Patel <yashpatel2165@gmail.com>
2025-03-20 10:44:12 +0530

    hii

--- travel-booking/src/App.js
+++ travel-booking/src/App.js
@@ -1,13 +1,44 @@
-import React from "react";
-import SignUp from "./components/signup"; // Import the SignUp component
-import "./App.css";
-
+import React from 'react';
+import './App.css';
 function App() {
+  const searchFunction = () => {
+    const input = document.getElementById('search').value;
+    alert(`Searching for: ${input}`);
+  };
   return (
-    <div className="App">
-      <SignUp /> {/* Display the SignUp component */}
+    <div className="container">
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <button className='login'>Log In</button>
+            </td>
+            <td>
+              <button className='signup'>Sign Up</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      {/* Search Bar */}
+      <div className="search-container">
+        <input type="text" id="search" placeholder="Search" />
+        <button onClick={searchFunction}>Go</button>
+      </div>
     </div>
   );
 }
-
+function Content(){
+  return(
+    <div className='list'>
+      <table>
+        <tr>
+          <td>
+            <img src='logo192.png'></img>
+          </td>
+        </tr>
+      </table>
+    </div>
+  )
+}
 export default App;
